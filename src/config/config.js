6ad2cc9fdbf4679e671eff59df5a26e007b12f41
Diego Collares <diego.collares@gmail.com>
2018-12-22 20:59:09 -0200

    config sequelize and pg

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'srdim',
+  password: 'srdim',
+  database: 'goBarber',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
