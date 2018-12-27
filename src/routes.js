346a40e0b1859e1ed3328c084a38236500984b54
Diego Collares <diego.collares@gmail.com>
2018-12-27 06:45:27 -0200

    Save user session storage file

--- src/routes.js
+++ src/routes.js
@@ -16,6 +16,9 @@ routes.get('/signup', UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
 // Dashboard
-routes.get('/app/dashboard', (req, res) => res.render('dashboard'))
+routes.get('/app/dashboard', (req, res) => {
+  console.log(req.session.user)
+  res.render('dashboard')
+})
 
 module.exports = routes
